393b07dc6ad54050c9a30d7947928c24e4108775
BRFlores <bryan.flores@epn.edu.ec>
2019-05-04 02:34:35 -0500

    Primer commit

---
+++ src/picoyplaca/PicoYPlaca.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package picoyplaca;
+
+/**
+ *
+ * @author ASUS
+ */
+public class PicoYPlaca {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         new PaginaPrincipal().setVisible(true);//mostramos la pantalla principal
+    }
+    
+}
